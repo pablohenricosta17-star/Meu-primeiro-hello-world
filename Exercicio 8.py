d1d7da002bd531ddd023106eee2f4bf23f97c05d
pablohenricosta17-star <pablohenri.costa17@gmail.com>
2026-03-17 21:44:43 -0300

    Update Exercicio 8.py

--- Exercicio 8.py
+++ Exercicio 8.py
@@ -1,5 +1,1 @@
-#8 Calculadora de dias: Peça a idade de uma pessoa apenas em anos e exiba aproximadamente quantos dias ela já viveu (Considere anos de 365 dias).
 
-idade = int(input("Digite sua idade: "))
-dia = idade * 365
-print("Você viveu", dia, "dias") 
